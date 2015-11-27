4d42cfa10dc920a7f99a3f87bd1c0e29a959dfce
Bogdasya <bogdasya93@gmail.com>
2015-11-28 00:12:54 +0200

    Adding tests

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -3,6 +3,12 @@ require 'test_helper'
 class ProductsControllerTest < ActionController::TestCase
   setup do
     @product = products(:one)
+      @update = {
+          title: "Test Test",
+          description: "QWErty",
+          image_url: "test.jpg",
+          price: 145.2
+      }
   end
 
   test "should get index" do
@@ -14,36 +20,5 @@ class ProductsControllerTest < ActionController::TestCase
   test "should get new" do
     get :new
     assert_response :success
-  end
-
-  test "should create product" do
-    assert_difference('Product.count') do
-      post :create, product: {  }
-    end
-
-    assert_redirected_to product_path(assigns(:product))
-  end
-
-  test "should show product" do
-    get :show, id: @product
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, id: @product
-    assert_response :success
-  end
-
-  test "should update product" do
-    patch :update, id: @product, product: {  }
-    assert_redirected_to product_path(assigns(:product))
-  end
-
-  test "should destroy product" do
-    assert_difference('Product.count', -1) do
-      delete :destroy, id: @product
-    end
-
-    assert_redirected_to products_path
   end
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Invalid params for new product" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
+  end
+
+  test "Invalid price" do
+    product = Product.new(title: "Test", description: "qwerty", image_url: "test.jpg")
+    product.price = -1
+    assert product.invalid?
+    product.price = 5
+    assert product.invalid?
+  end
 end
