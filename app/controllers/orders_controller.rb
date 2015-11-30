969bdff21bb13bd040c28247946fe6fb1ea3b11a
Bogdasya <bogdasya93@gmail.com>
2015-12-01 00:27:00 +0200

    Adding admin

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,7 @@
 class OrdersController < ApplicationController
+  include CurrentCart
+  skip_before_action :authorize, only: [:new, :create]
+  before_action :set_cart, only: [:new, :create]
   before_action :set_order, only: [:show, :edit, :update, :destroy]
 
   # GET /orders
@@ -14,6 +17,10 @@ class OrdersController < ApplicationController
 
   # GET /orders/new
   def new
+    if @cart.line_items.empty?
+      redirect_to store_url, notice: "Your cart is empty"
+      return
+    end
     @order = Order.new
   end
 
@@ -21,18 +28,23 @@ class OrdersController < ApplicationController
   def edit
   end
 
-  # POST /orders
-  # POST /orders.json
   def create
     @order = Order.new(order_params)
+    @order.add_line_items_from_cart(@cart)
 
     respond_to do |format|
       if @order.save
-        format.html { redirect_to @order, notice: 'Order was successfully created.' }
-        format.json { render :show, status: :created, location: @order }
+        Cart.destroy(session[:cart_id])
+        session[:cart_id] = nil
+        format.html { redirect_to store_url, notice:
+                                             'Thank you for your order.' }
+        format.json { render action: 'show', status: :created,
+                           location: @order }
       else
-        format.html { render :new }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
+        @cart = current_cart
+        format.html { render action: 'new' }
+        format.json { render json: @order.errors,
+                           status: :unprocessable_entity }
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,32 @@
 Rails.application.routes.draw do
 
+  get 'admin' => 'admin#index'
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  resources :users
   resources :orders
   root to: 'store#index', as: 'store'
 
   resources :line_items
   resources :carts
   resources :products
+  resources :products do
+    get :who_bought, on: :member
+  end
 
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store', via: :all
+  end
 end
